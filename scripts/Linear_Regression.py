e404f64f0008176760eb678b0871d6fb3ac833dd
Shaurya-47 <61658860+Shaurya-47@users.noreply.github.com>
2021-01-03 12:29:36 +0100

    Update Linear_Regression.py

--- scripts/Linear_Regression.py
+++ scripts/Linear_Regression.py
@@ -7,18 +7,9 @@ Created on Thu Dec 31 12:24:27 2020
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import RandomizedSearchCV
 from datetime import datetime
 import pickle as pkl
-
-#%%
-# Control panel
-scoreNormalized = False
-featureNormalized = True
+from sklearn import preprocessing
 
 #%% Preprocessing
 # Load data
@@ -41,52 +32,79 @@ X_tr = X_tr.drop("ID", axis = 1)
 y_t = y_t.drop("ID", axis = 1)
 y_tr = y_tr.drop("ID", axis = 1)
 
-# # correcting a mistake - encoding the time signature variable as dummy
-# X_tr.info()
-# X_tr.time_signature.replace((1,0.75,1.25,0.25,0), ('4/4','3/4','5/4','1/4','0/4'), inplace=True)
-# X_tr["time_signature"].value_counts()
-# dum_df = pd.get_dummies(X_tr["time_signature"], columns=["time_signature"], prefix="time_sig_is")
-# X_tr = X_tr.join(dum_df)
-# X_tr = X_tr.drop(["time_signature"],1)
+# scaling the inputs - do it only for the continuous variables
+X_tr_scaled = pd.DataFrame(preprocessing.scale(X_tr[['acousticness','danceability',
+                                                     'duration_ms','energy',
+                                                     'instrumentalness','liveness',
+                                                     'loudness','speechiness','tempo',
+                                                     'valence']]))
 
-# X_t.time_signature.replace((1,0.75,1.25,0.25,0), ('4/4','3/4','5/4','1/4','0/4'), inplace=True)
-# X_t['time_signature'] = X_t['time_signature'].astype(object)
-# dum_df = pd.get_dummies(X_t["time_signature"], columns=["time_signature"], prefix="time_sig_is")
-# X_t = X_t.join(dum_df)
-# X_t = X_t.drop(["time_signature"],1)
+# keeping the column names
+X_tr_scaled.columns = ['acousticness','danceability','duration_ms','energy',
+                       'instrumentalness','liveness','loudness','speechiness',
+                       'tempo','valence']
 
-# # removing the extra dummy to avoid the dummy variable trap
-# X_tr = X_tr.drop(columns=['time_sig_is_0/4'])
-# X_t = X_t.drop(columns=['time_sig_is_0/4'])
+# adding the other columns (in their original form) to the scaled data
+X_tr_scaled = pd.concat([X_tr_scaled, X_tr.iloc[:, 12:42]], axis=1)
 
-# # correcting another mistake - removing an extra dummy variable in Genre
-# X_tr = X_tr.drop(columns=['genre_is_A Capella'])
-# X_t = X_t.drop(columns=['genre_is_A Capella'])
+# adding the 2 remaining columns
+X_tr_scaled = pd.concat([X_tr_scaled, X_tr[['key', 'Mode is Major']]], axis=1)
 
-# Normalize popularity score
-if scoreNormalized == True:
-    
-    # Calculate mean and std for the training set
-    trainStd = np.std(y_tr["popularity"])
-    trainMean = np.mean(y_tr["popularity"])
-    
-    # Normalize training set and test set based on training set parameter
-    y_tr["popularity"] = (y_tr["popularity"]  - trainMean) / trainStd
-    y_t["popularity"] = (y_t["popularity"]  - trainMean) / trainStd
- 
-# Normalize features   
-if featureNormalized == True: 
-    # Calculate mean and std for the training set
-    scaler = StandardScaler().fit(X_tr.loc[:,"acousticness":"valence"])
-    
-    # Normalize training set
-    X_tr.loc[:,"acousticness":"valence"] = scaler.transform(X_tr.loc[:,"acousticness":"valence"])
-    
-    # Normalize test set based on training set param eter
-    X_t.loc[:,"acousticness":"valence"] = scaler.transform(X_t.loc[:,"acousticness":"valence"])
-    
-# Convert column into 1D array (for training labels)
-y_tr = np.array(y_tr).ravel()
+# importing models and evaluation metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import make_scorer
+
+# running linear regression without regularisation
+linreg = LinearRegression()
+
+# cross validation scores; 5 folds by default
+print(np.mean(cross_val_score(linreg, X_tr, y_tr, scoring=('neg_mean_squared_error'))), 
+      np.std(cross_val_score(linreg, X_tr, y_tr, scoring=('neg_mean_squared_error')))) 
+
+print(np.mean(cross_val_score(linreg, X_tr, y_tr, scoring=('r2'))), 
+      np.std(cross_val_score(linreg, X_tr, y_tr, scoring=('r2')))) 
+
+############# Maybe this can be done after the next milestone ####################################
+
+# maybe calculate r square too
+
+# https://scikit-learn.org/stable/auto_examples/model_selection/plot_randomized_search.html#sphx-glr-auto-examples-model-selection-plot-randomized-search-py
+# https://scikit-learn.org/stable/modules/grid_search.html
+
+# regul;arised models will always perform worse as they are trying to prevent overfitting
+
+# running linear regression with l2 regularisation - ridge
+ridge = Ridge(alpha=0.1)
+print(np.mean(cross_val_score(ridge, X_tr, y_tr, scoring=('neg_mean_squared_error'))), 
+      np.std(cross_val_score(ridge, X_tr, y_tr, scoring=('neg_mean_squared_error')))) 
+print(np.mean(cross_val_score(ridge, X_tr, y_tr, scoring=('r2'))), 
+      np.std(cross_val_score(ridge, X_tr, y_tr, scoring=('r2'))))
+
+# running linear regression with l1 regularisation - lasso
+lasso = Lasso(alpha=0.1)
+print(np.mean(cross_val_score(lasso, X_tr, y_tr, scoring=('neg_mean_squared_error'))), 
+      np.std(cross_val_score(lasso, X_tr, y_tr, scoring=('neg_mean_squared_error')))) 
+print(np.mean(cross_val_score(lasso, X_tr, y_tr, scoring=('r2'))), 
+      np.std(cross_val_score(lasso, X_tr, y_tr, scoring=('r2'))))
+
+# running linear regression with l1 and l2 both - elasticnet
+elasticnet = ElasticNet(alpha=0.1, l1_ratio=0.5)
+print(np.mean(cross_val_score(lasso, X_tr, y_tr, scoring=('neg_mean_squared_error'))), 
+      np.std(cross_val_score(lasso, X_tr, y_tr, scoring=('neg_mean_squared_error')))) 
+print(np.mean(cross_val_score(lasso, X_tr, y_tr, scoring=('r2'))), 
+      np.std(cross_val_score(lasso, X_tr, y_tr, scoring=('r2'))))
+# recursive feature elimination
+
+# NOTE: all models must be cross-validated to test performance and tune hyperparameters (random se)
+
+
 
 
 #%% Grid search
