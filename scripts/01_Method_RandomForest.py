0a2997767c6dadce091cb0ff1963b8fa7ff38f54
Chan Le <93.chan.le@gmail.com>
2021-01-03 12:47:08 +0100

    updated normalization script in RF

--- scripts/01_Method_RandomForest.py
+++ scripts/01_Method_RandomForest.py
@@ -14,6 +14,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import cross_val_score
 from datetime import datetime
 import pickle as pkl
 
@@ -29,19 +30,6 @@ X_tr = pd.read_csv("data/X_tr.csv")
 y_t = pd.read_csv("data/y_t.csv")
 y_tr = pd.read_csv("data/y_tr.csv")
 
-# Rename ID column
-for data in [X_t, X_tr, y_t, y_tr]:
-    data = data.rename(columns= {"Unnamed: 0":"ID"}, inplace= True)
-
-# Set ID column aside
-ID_t = X_t["ID"]
-ID_tr = X_tr["ID"]
-
-# Drop ID column 
-X_t = X_t.drop("ID", axis = 1)
-X_tr = X_tr.drop("ID", axis = 1)
-y_t = y_t.drop("ID", axis = 1)
-y_tr = y_tr.drop("ID", axis = 1)
 
 # Normalize popularity score
 if scoreNormalized == True:
@@ -56,13 +44,15 @@ if scoreNormalized == True:
  
 # Normalize features   
 if featureNormalized == True: 
+    #needNormalize = ["duration_ms", "energy", "instrumentalness", "key", "loudness", "tempo"]
+    
     # Calculate mean and std for the training set
     scaler = StandardScaler().fit(X_tr.loc[:,"acousticness":"valence"])
     
     # Normalize training set
     X_tr.loc[:,"acousticness":"valence"] = scaler.transform(X_tr.loc[:,"acousticness":"valence"])
     
-    # Normalize test set based on training set param eter
+    # Normalize test set based on training set parameter
     X_t.loc[:,"acousticness":"valence"] = scaler.transform(X_t.loc[:,"acousticness":"valence"])
     
 # Convert column into 1D array (for training labels)
@@ -73,16 +63,16 @@ y_tr = np.array(y_tr).ravel()
 # Prepare grid hyperparameters
 
 # Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
+n_estimators = [int(x) for x in np.linspace(start = 100, stop = 2000, num = 10)]
 # Number of features to consider at every split
 max_features = ['auto', 'sqrt']
 # Maximum number of levels in tree
 max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
 max_depth.append(None)
 # Minimum number of samples required to split a node
-min_samples_split = [5, 10, 20, 50]
+min_samples_split = [50, 100, 200, 500]
 # Minimum number of samples required at each leaf node
-min_samples_leaf = [2, 4, 10]
+min_samples_leaf = [20, 40, 100]
 # Method of selecting samples for training each tree
 bootstrap = [True, False]
 
@@ -94,13 +84,27 @@ random_grid = {'n_estimators': n_estimators,
                'min_samples_leaf': min_samples_leaf,
                'bootstrap': bootstrap}
 
+#%% Run base model
+RFBase = RandomForestRegressor(n_estimators = 1000, max_features = "sqrt", bootstrap = True, random_state = 2312, verbose = 3)
+
+# Cross validation
+
+RFModel = RFBase.fit(X_tr, y_tr)
+cross_val_score(RFBase, X_tr, y_tr, scoring = ("neg_mean_squared_error")) 
+
+
+
+# Prediction and check error
+prediction = RFModel.predict(X_t)
+RSME = mean_squared_error(y_t, prediction).round(2)
+print(RSME)
+
 #%% Excecuting grid search
 # Base model for tuning
 RFBase = RandomForestRegressor()
-
 # Random search of parameters, using 3 fold cross validation, 
 # search across 100 different combinations, and use all available cores
-RFGridModel = RandomizedSearchCV(estimator = RFBase, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+RFGridModel = RandomizedSearchCV(estimator = RFBase, param_distributions = random_grid, n_iter = 100, cv = 5, verbose=2, random_state=42, n_jobs = -1)
 
 # Execute grid search
 RFGridModel.fit(X_tr, y_tr)
@@ -122,3 +126,14 @@ now = datetime.now().strftime("%d-%m-%Y_%Hh%Mm")
 with open(f"models/RF_{now}_BestRMSE_{RSME}.pickle", "wb") as f:
     pkl.dump(RFGridModel, f)  
 
+#%%
+baseLine = {"baseLine" : np.repeat(np.mean(y_t), 46545)}
+baseLine = pd.DataFrame(data = baseLine  )
+
+mean_squared_error(y_t, baseLine).round(2)
+
+#%%
+baseLine = {"baseLine" : np.repeat(41, 46545)}
+baseLine = pd.DataFrame(data = baseLine)
+
+mean_squared_error(y_t, baseLine).round(2)
